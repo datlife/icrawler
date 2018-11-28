2ab4ece23ee52f1d2abcc3657a96239b26c8eba5
Dat Nguyen <tdat.nguyen93@gmail.com>
2018-11-28 11:28:47 -0800

    Updated download_files status

--- icrawler/crawler.py
+++ icrawler/crawler.py
@@ -186,15 +186,21 @@ class Crawler(object):
                          self.downloader.thread_num)
         self.downloader.start(**downloader_kwargs)
 
+        downloaded_files = 0
         progress_bar = tqdm.tqdm(total=downloader_kwargs['max_num'])
         while True:
-            progress_bar.update(self.downloader.fetched_num)
             if not self.feeder.is_alive():
                 self.signal.set(feeder_exited=True)
             if not self.parser.is_alive():
                 self.signal.set(parser_exited=True)
             if not self.downloader.is_alive():
+                progress_bar.close()
                 break
+            else:
+                downloaded_files = self.downloader.fetched_num - downloaded_files
+                progress_bar.update(downloaded_files)
+                downloaded_files = self.downloader.fetched_num
+
             time.sleep(1)
 
         if not self.feeder.in_queue.empty():
